1f50206dbc77fd35942257d2bcb8e80cb0ab92c4
Xavier Decuyper <xavier.decuyper@gmail.com>
2017-03-01 14:16:02 +0100

    Check if duplicity is installed

--- script.sh
+++ script.sh
@@ -5,9 +5,16 @@ cd /lxc-host/
 # Used to encrypt backups
 export PASSPHRASE="xxxxxxx"
 
+# Backblaze B2 configuration variables
 B2_BUCKET="xxxxxxx"
 B2_ACCOUNT="xxxxxxx"
 B2_KEY="xxxxxxx"
+
+# Check if Duplicity is installed
+if ! [ $(which duplicity 2>/dev/null) ]; then
+  echo "Duplicity is not installed. Can't continue."
+  exit 1;
+fi
 
 for dir in */
 do
